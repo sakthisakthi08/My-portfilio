d36c5ceec5be67ef512717e6a980c8d5be94a7c7
sakthi <ksakthictbecse@gmail.com>
2025-12-11 21:09:16 +0530

    change tha css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Intro from './Intro'
-import Skills from './Skil'
-import Project from './Project'
-import Resume from './Resume'
-import Contact from './Contact'
+import Navbar from "./Navbar";
+import Intro from "./Intro";
+import Skills from "./Skil";
+import Project from "./Project";
+import Resume from "./Resume";
+import Contact from "./Contact";
+
 function App() {
   return (
     <>
-      <div><Navbar /></div>
-      <div><Intro /></div>
-      <div><Skills/></div>
-      <div><Project/></div>
-      <div><Resume/></div>
-      <div><Contact/></div>
+      <Navbar />
+      <Intro />
+      <Skills />
+      <Project />
+      <Resume />
+      <Contact />
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
